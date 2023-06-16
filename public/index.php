b5aeda593f437345707a338fef7bcb0c2baab357
lynvs <lynette@gobuild.co.za>
2023-06-16 14:10:17 +0200

    updating index to allow use of global variables, updating env and env example files

--- public/index.php
+++ public/index.php
@@ -2,5 +2,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Coolblue\Interview\ShoppingCart;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 echo (new ShoppingCart())->render();
